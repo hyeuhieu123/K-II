4d9d569ad8b83d431f897ec8b1fb998f91933437
hieu <nguyentrunghieuthptxp@gmail.com>
2024-05-08 12:38:10 +0700

    add

--- ss30+31/src/bt/Bt1.tsx
+++ ss30+31/src/bt/Bt1.tsx
@@ -1,19 +1,20 @@
-import React, { Component } from 'react'
-interface Props{
-    userName:string
+import React, { Component } from "react";
+interface Props {
+  userName: string;
 }
-export default class Bt1 extends Component<{},Props> {
-    constructor(prop:Props) {
-        super(prop);
-        this.state = {
-            userName: "Nam đẹp trai"
-        }
-    }
+export default class Bt1 extends Component<{}, Props> {
+  constructor(prop: Props) {
+    super(prop);
+    this.state = {
+      userName: "trung hieu",
+    };
+  }
   render() {
     return (
-        <div>Bt1:
-            <p>Chào { this.state.userName}</p>
+      <div>
+        Bt1:
+        <p>Chào {this.state.userName}</p>
       </div>
-    )
+    );
   }
 }
