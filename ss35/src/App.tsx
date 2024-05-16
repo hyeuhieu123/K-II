f9d5f164d6137c5a7e44ef8d26902bc2fddd9f7b
hieu <nguyentrunghieuthptxp@gmail.com>
2024-05-16 13:15:53 +0700

    add

---
+++ ss35/src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      {/* hook:móc
+        bản chất là những hàm, phương thức giúp cho function component có thể thực hiện dc các tính năng y hệt class component
+        +react hook được ra đời ở phiên bản react 16.8
+        +trước khi hook ra đời thì function component chỉ đơn giản là tạo ra các UI
+        function Header(){
+          return <>header<>
+        }
+        +hook ra đời giúp cho code ngắn gọn dễ dàng sử dụng hơn so với class component
+        +dự án bây giờ đa phần sử dụng react-hook
+        +sau này đi làm có những dự án vẫn đang dùng class (maintain-chỉnh sửa)
+        +khi làm việc với class thì phải nắm liècycle
+        +khi làm việc với các hook phải để ý đến đầu vào đầu ra của các phương thức 
+        +các hook học
+            +useState
+            +userEffect
+            +useRef
+            +useMemo
+            +useRenducer
+            +useCallback
+      */}
+    </div>
+  );
+}
